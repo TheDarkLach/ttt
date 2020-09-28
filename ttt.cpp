e432b0e0de953fcef5d0bf8b89cde842c4fde692
TheDarkLach <11thunderstorm11@gmail.com>
2020-09-27 18:49:07 -0700

    almost there

--- ttt.cpp
+++ ttt.cpp
@@ -4,17 +4,75 @@ using namespace std;
 
 int main()
 {
-  int checkWin();
+
+  int checkwin();
   void board();
+  int square[3][3];
 
   int player = 1, i, choice;
 
-  char mark;
+    char mark;
+    do
+    {
+        board();
+        
+        player = (player % 2) ? 1 : 2;
 
-  do
-  {
+        cout << "Player " << player << ", enter a number:  ";
+        cin >> choice;
+
+        mark=(player == 1) ? 'X' : 'O';
+
+        if (choice == 1 && square[1][1] == '1')
+
+            square[1][1] = mark;
+        else if (choice == 2 && square[2][1] == '2')
+
+            square[2][1] = mark;
+        else if (choice == 3 && square[3][1] == '3')
+
+            square[3][1] = mark;
+        else if (choice == 4 && square[1][2] == '4')
+
+            square[1][2] = mark;
+        else if (choice == 5 && square[2][2] == '5')
+
+            square[2][2] = mark;
+        else if (choice == 6 && square[3][2] == '6')
+
+            square[3][2] = mark;
+        else if (choice == 7 && square[1][3] == '7')
+
+            square[1][3] = mark;
+        else if (choice == 8 && square[2][3] == '8')
+
+            square[2][3] = mark;
+        else if (choice == 9 && square[3][3] == '9')
+
+            square[3][3] = mark;
+        else
+        {
+            cout<<"Invalid move ";
+
+            //go back to the turn
+            player--;
+            cin.ignore();
+            cin.get();
+        }
+        i=checkwin();
+
+        player++;
+    }while(i==-1);
     board();
-  }while (i == -1);
+    if(i==1)
+
+        cout<<"\nPlayer "<<--player<<" win ";
+    else
+        cout<<"\nGame draw";
+
+    cin.ignore();
+    cin.get();
+    return 0;
 }
 
 int checkwin(int square[3][3])
