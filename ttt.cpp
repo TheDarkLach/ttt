9f7a093db0104de40830bde9e721cade4679f5fd
TheDarkLach <11thunderstorm11@gmail.com>
2020-09-28 10:39:03 -0700

    hard without global v

--- ttt.cpp
+++ ttt.cpp
@@ -1,15 +1,90 @@
 #include <iostream>
-
 using namespace std;
+
+int checkwin(int square[3][3])
+{
+    //row 1
+    if (square[0][0] == square[1][0] && square[1][0] == square[2][0])
+
+        return 1;
+    //row 2
+    else if (square[0][1] == square[1][1] && square[1][1] == square[2][1])
+
+        return 1;
+    //row 3
+    else if (square[2][0] == square[2][1] && square[2][1] == square[2][2])
+
+        return 1;
+    //column 1
+    else if (square[0][0] == square[0][1] && square[0][1] == square[0][2])
+
+        return 1; 
+    //column 2
+    else if (square[1][0] == square[1][1] && square[1][1] == square[1][2])
+
+        return 1;
+    //column 3
+    else if (square[2][0] == square[2][1] && square[2][1] == square[2][2])
+
+        return 1;
+
+    //diagonals
+    else if (square[0][0] == square[1][1] && square[1][1] == square[2][2])
+
+        return 1;
+    else if (square[2][0] == square[1][1] && square[1][1] == square[0][2])
+
+        return 1;
+    else if (square[0][0] != '1' && square[1][0] != '2' && square[2][0] != '3' 
+                    && square[0][1] != '4' && square[1][1] != '5' && square[2][1] != '6' 
+                  && square[0][2] != '7' && square[1][2] != '8' && square[2][2] != '9')
+
+        return 0;
+    else
+        return -1;
+}
+
+
+/*******************************************************************
+     FUNCTION TO DRAW BOARD OF TIC TAC TOE WITH PLAYERS MARK
+********************************************************************/
+
+
+void board(int square[3][3])//oid showBoard(string board[3][3])
+{
+
+    //system("cls");
+    //cout << "\n\n\tTic Tac Toe\n\n";
+
+    cout << "Player 1 (X)  -  Player 2 (O)" << endl << endl;
+    cout << endl;
+
+    cout << "     |     |     " << endl;
+    cout << "  " << square[0][0] << "  |  " << square[1][0] << "  |  " << square[2][0] << endl;
+
+    cout << "_____|_____|_____" << endl;
+    cout << "     |     |     " << endl;
+
+    cout << "  " << square[0][1] << "  |  " << square[1][1] << "  |  " << square[2][1] << endl;
+
+    cout << "_____|_____|_____" << endl;
+    cout << "     |     |     " << endl;
+
+    cout << "  " << square[0][2] << "  |  " << square[1][2] << "  |  " << square[2][2] << endl;
+
+    cout << "     |     |     " << endl << endl;
+}
+
 
 int main()
 {
 
   int checkwin();
   void board();
+
   int square[3][3];
 
-  int player = 1, i, choice;
+	int player = 1, i, choice;
 
     char mark;
     do
@@ -23,33 +98,33 @@ int main()
 
         mark=(player == 1) ? 'X' : 'O';
 
-        if (choice == 1 && square[1][1] == '1')
+        if (choice == 1 && square[0][0] == '1')
+
+            square[0][0] = mark;
+        else if (choice == 2 && square[1][0] == '2')
+
+            square[1][0] = mark;
+        else if (choice == 3 && square[2][0] == '3')
+
+            square[2][0] = mark;
+        else if (choice == 4 && square[0][1] == '4')
+
+            square[0][1] = mark;
+        else if (choice == 5 && square[1][1] == '5')
 
             square[1][1] = mark;
-        else if (choice == 2 && square[2][1] == '2')
+        else if (choice == 6 && square[2][1] == '6')
 
             square[2][1] = mark;
-        else if (choice == 3 && square[3][1] == '3')
+        else if (choice == 7 && square[0][2] == '7')
 
-            square[3][1] = mark;
-        else if (choice == 4 && square[1][2] == '4')
+            square[0][2] = mark;
+        else if (choice == 8 && square[1][2] == '8')
 
             square[1][2] = mark;
-        else if (choice == 5 && square[2][2] == '5')
+        else if (choice == 9 && square[2][2] == '9')
 
             square[2][2] = mark;
-        else if (choice == 6 && square[3][2] == '6')
-
-            square[3][2] = mark;
-        else if (choice == 7 && square[1][3] == '7')
-
-            square[1][3] = mark;
-        else if (choice == 8 && square[2][3] == '8')
-
-            square[2][3] = mark;
-        else if (choice == 9 && square[3][3] == '9')
-
-            square[3][3] = mark;
         else
         {
             cout<<"Invalid move ";
@@ -66,67 +141,11 @@ int main()
     board();
     if(i==1)
 
-        cout<<"\nPlayer "<<--player<<" win ";
+        cout<<"==>\aPlayer "<<--player<<" win ";
     else
-        cout<<"\nGame draw";
+        cout<<"==>\aGame draw";
 
     cin.ignore();
     cin.get();
     return 0;
-}
-
-int checkwin(int square[3][3])
-{
-
-    if (square[1][1] == square[2][1] && square[2][1] == square[3][1])
-
-        return 1;
-    else if (square[2][1] == square[2][2] && square[2][2] == square[2][3])
-
-        return 1;
-    else if (square[3][1] == square[3][2] && square[3][2] == square[3][3])
-
-        return 1;
-    else if (square[1][1] == square[1][2] && square[1][2] == square[1][3])
-
-        return 1;
-    else if (square[2][1] == square[2][2] && square[2][2] == square[2][3])
-
-        return 1;
-    else if (square[3][1] == square[3][2] && square[3][2] == square[3][3])
-
-        return 1;
-    else if (square[1][1] == square[2][2] && square[2][2] == square[3][3])
-
-        return 1;
-    else if (square[3][1] == square[2][2] && square[2][2] == square[1][3])
-
-        return 1;
-    else if (square[1][1] != '1' && square[2][1] != '2' && square[3][1] != '3' 
-                    && square[1][2] != '4' && square[2][2] != '5' && square[3][2] != '6' 
-                  && square[3][1] != '7' && square[3][2] != '8' && square[3][3] != '9')
-
-        return 0;
-    else
-        return -1;
-}
-
-void board (int square[3][3])
-{
-   cout << "Player 1 (X) - Player 2 (O)" << endl << endl;
-
-   cout << "     |     |     " << endl;
-   cout << "  " << square[1][1] << "  |  " << square[2][1] << "  |  " << square[3][1] << endl;
-
-   cout << "_____|_____|_____" << endl;
-   cout << "     |     |     " << endl;
-
-   cout << "  " << square[2][1] << "  |  " << square[2][2] << "  |  " << square[2][3] << endl;
-
-   cout << "_____|_____|_____" << endl;
-   cout << "     |     |     " << endl;
-
-   cout << "  " << square[3][1] << "  |  " << square[3][2] << "  |  " << square[3][3] << endl;
-
-   cout << "     |     |     " << endl << endl;
 }
