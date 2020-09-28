8c2e32a4467b58c872e289bbf529b0c40929d889
TheDarkLach <11thunderstorm11@gmail.com>
2020-09-27 17:46:04 -0700

    check win added

--- ttt.cpp
+++ ttt.cpp
@@ -13,10 +13,45 @@ int main()
 
   do
   {
-    //board();
+    board();
   }while (i == -1);
 }
 
+int checkwin(int square[3][3])
+{
+
+    if (square[1][1] == square[2][1] && square[2][1] == square[3][1])
+
+        return 1;
+    else if (square[2][1] == square[2][2] && square[2][2] == square[2][3])
+
+        return 1;
+    else if (square[3][1] == square[3][2] && square[3][2] == square[3][3])
+
+        return 1;
+    else if (square[1][1] == square[1][2] && square[1][2] == square[1][3])
+
+        return 1;
+    else if (square[2][1] == square[2][2] && square[2][2] == square[2][3])
+
+        return 1;
+    else if (square[3][1] == square[3][2] && square[3][2] == square[3][3])
+
+        return 1;
+    else if (square[1][1] == square[2][2] && square[2][2] == square[3][3])
+
+        return 1;
+    else if (square[3][1] == square[2][2] && square[2][2] == square[1][3])
+
+        return 1;
+    else if (square[1][1] != '1' && square[2][1] != '2' && square[3][1] != '3' 
+                    && square[1][2] != '4' && square[2][2] != '5' && square[3][2] != '6' 
+                  && square[3][1] != '7' && square[3][2] != '8' && square[3][3] != '9')
+
+        return 0;
+    else
+        return -1;
+}
 
 void board (int square[3][3])
 {
