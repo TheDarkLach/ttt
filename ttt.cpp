3c2b87ee7fdb6bc5f722344f5cd9725ce542a530
TheDarkLach <11thunderstorm11@gmail.com>
2020-10-01 20:31:04 -0700

    comments + cleanup

--- ttt.cpp
+++ ttt.cpp
@@ -1,8 +1,12 @@
+//TicTacToe in C++ by Faizan Karim
+//codes pretty messy
 #include <iostream>
 using namespace std;
 
+//checkwin
 int checkwin(char square[3][3])
 {
+  //instead of having 1 for X and one for ), just check to see if valeus are same
     //row 1
     if (square[0][0] == square[1][0] && square[1][0] == square[2][0])
 
@@ -35,6 +39,7 @@ int checkwin(char square[3][3])
     else if (square[2][0] == square[1][1] && square[1][1] == square[0][2])
 
         return 1;
+    //draw
     else if (square[0][0] != '1' && square[1][0] != '2' && square[2][0] != '3' 
                     && square[0][1] != '4' && square[1][1] != '5' && square[2][1] != '6' 
                   && square[0][2] != '7' && square[1][2] != '8' && square[2][2] != '9')
@@ -42,9 +47,11 @@ int checkwin(char square[3][3])
         return 0;
     else
         return -1;
+    //1 means win, 0 means tie, -1 means none
 }
 
-
+//draw board function
+//the spacing took a while but i got it eventually
 void board(char square[3][3])
 {
     cout << "\nPlayer 1 (X)  -  Player 2 (O)" << endl << endl;
@@ -69,28 +76,37 @@ void board(char square[3][3])
 
 int main()
 {
+  //array for each square
   char square[3][3] = {'1','4','7','2','5','8','3','6','9'};
 
-	int player = 1, i, choice, xWins = 0, oWins = 0, ties = 0;
+  //all my ints, theres a lot of them
+  int player = 1, i, choice, xWins = 0, oWins = 0, ties = 0;
   int pls = 1;
 
+  //for marking X and O
   char mark;
+
+  //after every round, reassign values to each square with while
   while (pls == 1)
   {
     char square[3][3] = {'1','4','7','2','5','8','3','6','9'};
     i = -1;
+    //do this while no one wins/draws
     do
     {
+      //draw board
       board(square);
-        
+
+      //switch player between 1 and 2 to switch between X and O
       player = (player % 2) ? 1 : 2;
-      //hoperfully counts wins
 
       cout << "Player " << player << ", enter a number:  ";
       cin >> choice;
 
+      //did the same thing with the mark as player, switch between
       mark=(player == 1) ? 'X' : 'O';
 
+      //add marks while the square is not occupied
       if (choice == 1 && square[0][0] == '1')
 
         square[0][0] = mark;
@@ -127,10 +143,12 @@ int main()
         cin.ignore();
         cin.get();
       }
+      // checkwin
       i=checkwin(square);
 
       player++;
 
+      //winner
       if(i==1)
       {
         cout << "\nPlayer " << --player << " wins!\n";
@@ -149,8 +167,8 @@ int main()
           cout << "Ties: " << ties << endl;
         }
 
-      //cout<< "clear";
       }
+      //draw
       else if (i == 0)
       {
         ties++;
@@ -164,7 +182,7 @@ int main()
 
     }while(i==-1);
   }
-    
+  //reddraw board
     board(square);
 
     cin.ignore();
